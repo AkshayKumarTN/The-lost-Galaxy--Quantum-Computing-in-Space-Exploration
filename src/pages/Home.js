21cc3e95663a16933eff177556292f9af943d22c
james <jameswu6789@gmail.com>
2025-03-10 22:16:26 -0400

    Commit for scrum-1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,18 +5,34 @@ export default function Home() {
     <div style={{ textAlign: 'center', padding: '20px' }}>
       <h1>Welcome to The Lost Galaxy</h1>
       <p>Click the button below to start the game!</p>
+      
       <Link 
-        to="/game" 
+        to="/game"
         style={{
           display: 'inline-block',
           padding: '10px 20px',
           backgroundColor: '#007bff',
           color: '#fff',
           borderRadius: '5px',
-          textDecoration: 'none'
+          textDecoration: 'none',
+          marginRight: '10px'
         }}
       >
         Start Game
+      </Link>
+
+      <Link 
+        to="/tutorial"
+        style={{
+          display: 'inline-block',
+          padding: '10px 20px',
+          backgroundColor: '#28a745',
+          color: '#fff',
+          borderRadius: '5px',
+          textDecoration: 'none'
+        }}
+      >
+        Tutorial
       </Link>
     </div>
   );
