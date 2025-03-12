9c135322d1db518adc578c07fa32de72571753f8
AKSHAY KUMAR TALUR NARASIMMULU <tnak369@gmail.com>
2025-03-11 20:25:46 -0400

    Updated Home Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -27,8 +27,8 @@ export default function Home() {
   }, []);
 
   return (
-    <div style={{ padding: '10px 20px', background: 'black', minHeight: '100vh' }}>
-      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+    <div className="home-main">
+      <div className="home-header" style={{ display: 'flex', justifyContent: 'space-between' }}>
         <img src="/assets/images/The-Lost-Galaxy-Home-icon.png" alt="The Lost Galaxy" height="100" />
         <div style={{ display: 'flex', alignItems: 'center', gap: '1em', fontSize: '20px' }}>
           <a style={{ color: '#8bb2ff', textAlign: 'center', fontWeight: '400', textDecoration: 'none' }}>Log in</a>
@@ -36,7 +36,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div style={{ display: 'flex', flexFlow: 'column', padding: '20px', color: '#FFF', marginTop: '30px' }}>
+      <div className="home-body">
         <div className="dynamic-transform-absolute" style={{ right: '0' }}>
           <img src="/assets/images/alien spaceships ufo with blue.png" alt="UFO" height="800" />
         </div>
@@ -77,18 +77,122 @@ export default function Home() {
             }}>
               Learn More
             </Link>
+            {/* <div><img src="/assets/images/SP-Studio.png" alt="UFO" height="300" /></div> */}
           </div>
         </div>
-        <div>
-          <h1>What is Quantum Computing?</h1>
+        <div style={{ marginTop: '5em' }}>
+          <h1>About The Lost Galaxy</h1>
           <div className="dynamic-transform-absolute" style={{ right: '0' }}>
             <img src="/assets/images/home-cta-planet.png" alt="planet" height="400" />
           </div>
+          <div style={{ display: 'flex', alignItems: 'center', gap: '1em' }}>
+            <div>The Lost Galaxy is an epic space adventure game that takes you on a journey through uncharted galaxies,  <br />
+              ancient alien civilizations, and thrilling interstellar battles. As a fearless explorer, you'll navigate mysterious <br />
+              worlds,uncover hidden secrets, and battle cosmic threats to save the galaxy from impending doom. <br /> <br />
+              But there's more—your greatest weapon isn't just your starship, but your mastery of quantum computing.<br />
+              Solve complex quantum puzzles, crack alien codes, and use quantum algorithms to unlock technologies <br />
+              that can reshape your cosmic destiny.<br /><br />
+              Customize your starship, forge alliances, and make choices that shape your destiny.  <br />
+              Whether you crave intense space combat, deep exploration, or intriguing storylines,  <br />
+              The Lost Galaxy offers an unforgettable adventure among the stars. <br /> <br />
+              Are you ready to uncover the secrets of the cosmos? Your journey begins now.</div>
+          </div>
 
-          <div className="dynamic-transform-absolute" style={{ left: '0' }}>
-            <img src="/assets/images/home-cta-rocket.png" alt="rocket" width="288" />
+
+        </div>
+        <div style={{ margin: '10em 10em 0em 10em' }}>
+          <div style={{ display: 'flex', alignItems: 'center', gap: '10em' }}>
+            <div className="dynamic-transform" style={{ left: '0' }}>
+              <img src="/assets/images/home-cta-rocket.png" alt="rocket" width="288" />
+            </div>
+            <div>
+              <h1>What is Quantum Computing?</h1>
+              <div>Quantum Computing is a cutting-edge technology that uses the principles of quantum physics to process information. <br />
+                Unlike traditional computers that use bits (0s and 1s), quantum computers use qubits, which can represent multiple states at once.<br />
+                This allows them to solve complex problems much faster than classical computers, revolutionizing fields like cryptography, medicine, and artificial intelligence.</div>
+              <div style={{ marginTop: '2em', display: 'flex', alignItems: 'center', gap: '1em' }}>
+                <Link to="/game" style={{
+                  padding: '.5em 1em',
+                  color: '#8bb2ff',
+                  textAlign: 'center',
+                  fontSize: '1.25em',
+                  fontWeight: '600',
+                  border: '1px solid',
+                  borderColor: '#8bb2ff',
+                  textDecoration: 'none'
+                }}>
+                  Learn More
+                </Link>
+                {/* <div><img src="/assets/images/SP-Studio.png" alt="UFO" height="300" /></div> */}
+              </div>
+            </div>
           </div>
         </div>
+
+        <div style={{ margin: '10em 10em 0em 10em' }}>
+          <div style={{ display: 'flex', alignItems: 'center', gap: '15em' }}>
+          <div style={{ left: '0', position:'absolute' }}>
+              <img src="/assets/images/nebula.webp" alt="rocket" width="588" />
+            </div>
+            <div style={{marginLeft:'15em', zIndex:'1'}}>
+              <h1>What is Space Exploration?</h1>
+              <div>Space Exploration is the investigation and discovery of outer space using astronomy, spacecraft, and advanced technology. <br />
+                It involves studying planets, stars, and galaxies, as well as sending robots and astronauts beyond Earth to explore the universe. <br />
+                Space exploration helps us understand the origins of the cosmos, search for life beyond Earth, <br />
+                and develop new technologies for the future.
+
+              </div>
+              <div style={{ marginTop: '2em', display: 'flex', alignItems: 'center', gap: '1em' }}>
+                <Link to="/game" style={{
+                  padding: '.5em 1em',
+                  color: '#8bb2ff',
+                  textAlign: 'center',
+                  fontSize: '1.25em',
+                  fontWeight: '600',
+                  border: '1px solid',
+                  borderColor: '#8bb2ff',
+                  textDecoration: 'none'
+                }}>
+                  Learn More
+                </Link>
+                {/* <div><img src="/assets/images/SP-Studio.png" alt="UFO" height="300" /></div> */}
+              </div>
+            </div>
+            
+          </div>
+        </div>
+
+
+        <div style={{ margin: '10em 10em 0em 10em' }}>
+          <div style={{ display: 'flex', alignItems: 'center', gap: '10em' }}>
+            <div className="dynamic-transform" style={{ left: '0' }}>
+              <img src="/assets/images/home-cta-rocket.png" alt="rocket" width="288" />
+            </div>
+            <div>
+              <h1>What is Quantum Computing?</h1>
+              <div>Quantum Computing is a cutting-edge technology that uses the principles of quantum physics to process information. <br />
+                Unlike traditional computers that use bits (0s and 1s), quantum computers use qubits, which can represent multiple states at once.<br />
+                This allows them to solve complex problems much faster than classical computers, revolutionizing fields like cryptography, medicine, and artificial intelligence.</div>
+              <div style={{ marginTop: '2em', display: 'flex', alignItems: 'center', gap: '1em' }}>
+                <Link to="/game" style={{
+                  padding: '.5em 1em',
+                  color: '#8bb2ff',
+                  textAlign: 'center',
+                  fontSize: '1.25em',
+                  fontWeight: '600',
+                  border: '1px solid',
+                  borderColor: '#8bb2ff',
+                  textDecoration: 'none'
+                }}>
+                  Learn More
+                </Link>
+                {/* <div><img src="/assets/images/SP-Studio.png" alt="UFO" height="300" /></div> */}
+              </div>
+            </div>
+          </div>
+        </div>
+
+
       </div>
     </div>
   );
