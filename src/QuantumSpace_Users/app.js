54fc9377abcb8984218f5e7240c582ffb986e048
Zaafira Hasan <zfh4@njit.edu>
2025-03-13 18:28:48 -0400

    Added authentication integration

--- src/QuantumSpace_Users/app.js
+++ src/QuantumSpace_Users/app.js
@@ -1,20 +1,20 @@
 
-import { createUser, updatePassword } from './data/users.js';
-import { dbConnection, closeConnection } from './config/mongoConnection.js';
+// import { createUser, updatePassword } from './data/users.js';
+// import { dbConnection, closeConnection } from './config/mongoConnection.js';
 
 
-const db = await dbConnection();
-await db.dropDatabase();
+// const db = await dbConnection();
+// await db.dropDatabase();
 
-let user = undefined;
+// let user = undefined;
 
-try{
-user = await createUser('remese@gmail.com', 'rme101', "testing123");
-    console.log(user);
+// try{
+// user = await createUser('remese@gmail.com', 'rme101', "testing123");
+//     console.log(user);
 
-} catch(e) {
-    console.log(e);
-}
+// } catch(e) {
+//     console.log(e);
+// }
 
 
 
