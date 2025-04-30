beb72fb3606abcac5076cc271da8d7b6aaa4257a
Zeel Desai <zeeldesai0904@gmail.com>
2025-04-29 22:26:03 -0400

    Implement Level 3 door unlocking with secure key logic

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -3,13 +3,14 @@ import Phaser from 'phaser';
 import MainScene from '../game/scenes/MainScene';
 import Level1Scene from '../game/scenes/Level1Scene';
 import Level2Scene from '../game/scenes/Level2Scene';
+import Level3Scene from '../game/scenes/Level3Scene';
 
 const config = {
   type: Phaser.AUTO,
   width: window.innerWidth, //800
   height: window.innerHeight, //600,
   parent: 'game-container',
-  scene: [MainScene, Level1Scene, Level2Scene],  // Include all scenes
+  scene: [MainScene, Level1Scene, Level2Scene, Level3Scene],  // Include all scenes
   physics: {
     default: 'arcade',
     arcade: { gravity: { y: 0 } }
